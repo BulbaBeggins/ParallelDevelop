2eb358713e5119a0783413b85a4b78cf8e89a3c5
vtomko <Jester598022>
2021-09-15 20:49:36 +0300

    add test method  lastIndexOftest  in class TestString

--- src/test/java/TestString.java
+++ src/test/java/TestString.java
@@ -2,14 +2,21 @@ public class TestString {
     public static void main(String[] args) {
         splitTest();
         charAtTest();
+        lastIndexOftest();
     }
 
     public static void splitTest() {
         String[] expected = {"два", "слова"};
         Assertion.assertEquals("TestString.splitTest", expected, "два слова".split(" "));
     }
-    public static void charAtTest(){
+
+    public static void charAtTest() {
         char expected = 'о';
-        Assertion.assertEquals("TestString.charAtTest",expected,"абоба".charAt(2));
+        Assertion.assertEquals("TestString.charAtTest", expected, "абоба".charAt(2));
+    }
+
+    public static void lastIndexOftest() {
+        int expected = 4;
+        Assertion.assertEquals("TestString.lastIndexOftest", expected, "теспир".lastIndexOf('и'));
     }
 }
