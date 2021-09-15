d0245d47de61f57d14a655b5ace8376ebc51334e
BulbaBeggins <Bumerang>
2021-09-15 21:31:02 +0400

    Add Test method "charAtTest" in class TestString

--- src/test/java/TestString.java
+++ src/test/java/TestString.java
@@ -1,10 +1,15 @@
 public class TestString {
     public static void main(String[] args) {
         splitTest();
+        charAtTest();
     }
 
     public static void splitTest() {
         String[] expected = {"два", "слова"};
         Assertion.assertEquals("TestString.splitTest", expected, "два слова".split(" "));
+    }
+    public static void charAtTest(){
+        char expected = 'о';
+        Assertion.assertEquals("TestString.charAtTest",expected,"абоба".charAt(2));
     }
 }
